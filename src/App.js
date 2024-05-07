698aa9ec13a025c723ab901cd1f4195ce12b78a6
shekh <shekharsushant54@gmail.com>
2024-05-07 16:58:26 +0530

    checkout page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import { SignupPage } from "./pages/SignupPage";
 import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
 
 
 import "./App.css";
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage></CartPage>,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout></Checkout>,
   },
 ]);
 
