695ea939abbe2e6635ecc04d3a42648f2105e929
shekh <shekharsushant54@gmail.com>
2024-05-09 13:10:42 +0530

    sorting in Product list

--- src/features/product-list/ProductAPI.js
+++ src/features/product-list/ProductAPI.js
@@ -1,25 +1,36 @@
 export function fetchAllProducts() {
-    return new Promise(async (resolve) => {
-      //TODO --> we will not hard code to the server URL here
-      const response = await fetch('http://localhost:8080/products');
-      const data = await response.json();
-      resolve(data);
-    });
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products') 
+    const data = await response.json()
+    resolve(data)
   }
+  );
+}
 
-
-export function fetchProductsByFilters(filter) {
-    //filter object = {"category":"laptops"}
-    //TODO : on server we will support multiple values
-    let queryString = '';
-    for(let key in filter){
-        queryString+=`${key}=${filter[key]}&`;
+export function fetchProductsByFilters(filter,sort) {
+  //Todos in the later section
+  // filter = {"category":"smartphone","laptops"}
+  // sort = {_sort:"price",_order="desc"}
+  // TODO : on server we will support multi values
+  let queryString = '';
+  for(let key in filter){
+    const categoryValues = filter[key];
+    if(categoryValues.length>0){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
     }
-    
-    return new Promise(async (resolve) => {
-       const response = await fetch('http://localhost:8080/products?'+queryString);
-      const data = await response.json();
-      resolve(data);
-    });
   }
-  
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
+
+  }
+
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products?'+queryString) 
+    const data = await response.json()
+    resolve(data)
+  }
+  );
+}
